2eb1f9f4063ff3a931ca9487450ec51fe5788088
Brannnt <123214814+Brannnt@users.noreply.github.com>
2023-03-27 23:09:21 -0600

    week 9
    week 9

---
+++ Homework-9/A9 Page/sketch.js
@@ -0,0 +1,27 @@
+    function setup()
+    {
+        createCanvas (600, 600);
+    }
+    function draw()
+    {
+        background (120);  
+        fill(255,204,0);
+        circle(300,200,200);
+        fill(1000)
+        circle(330,180,30);
+        circle(270,180,30);
+        fill(0)
+        circle(330,180,10);
+        circle(270,180,10);
+        fill(100)
+        line(300,220,180,270);
+        line(300,220,420,270);
+        rect(225,300,150,200);
+        fill(255,204,0);
+        ellipse(200,350,100,70);
+        ellipse(400,350,100,70);
+        fill(0)
+        triangle(225,500,270,500,230,550);
+        triangle(375,500,330,500,370,550);
+        Point(300,300);
+    }
